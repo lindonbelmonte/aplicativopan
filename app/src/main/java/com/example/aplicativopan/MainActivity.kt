b56f2ab639f03e32c4e5cf27d1c1cfc0b540496c
pcetec <pc@etec.com>
2026-09-23 20:13:52 -0300

    calculadora de imc

--- app/src/main/java/com/example/aplicativopan/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopan/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.aplicativopan
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,16 +12,45 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.aulaid)
 
-        val botao = findViewById<Button>(R.id.btnProxima)
-        val texto = findViewById<EditText>(R.id.editValor)
-
-        botao.text = "Novo Texto"
+        val notaP1 = findViewById<EditText>(R.id.editNota1)
+        val notaP2 = findViewById<EditText>(R.id.editNota2)
+        val resultado = findViewById<TextView>(R.id.txtResultado)
+        val botao = findViewById<Button>(R.id.btnCalcular)
 
         botao.setOnClickListener {
-            botao.text = texto.text
+            resultado.text = "Media é: " + ((notaP1.text.toString().toFloat()
+                    + notaP2.text.toString().toFloat()) / 2).toString()
 
         }
+
+        val peso = findViewById<EditText>(R.id.editPeso)
+        val altura = findViewById<EditText>(R.id.editAltura)
+        val resultadoimc = findViewById<TextView>(R.id.txtResultadoImc)
+        val botaoimc = findViewById<Button>(R.id.btnImc)
+
+        botaoimc.setOnClickListener {
+            val multiplica = (altura.text.toString().toFloat() * altura.text.toString().toFloat())
+            val imc = (peso.text.toString().toFloat()) / multiplica
+            resultadoimc.text = imc.toString()
+
+            if (imc < 18.5)
+            {
+                resultadoimc.text = imc.toString() + " Abaixo do peso"
+
+            } else if (imc >= 18.5 && imc <= 24.9)
+            {
+                resultadoimc.text = imc.toString() +  " sobrepeso"
+            }
+            else
+            {
+                resultadoimc.text = imc.toString() + " Obesidade"
+            }
+        }
+
+
+
     }
 }
